8fe1e3d50565c8591839da7dfddedba8824ee1af
西村勇希 <coca.xnhp@gmail.com>
2016-11-28 12:16:02 +0900

    レビューページ修正

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -59,11 +59,36 @@
                 <form method="post" action="{{url('/review/')}}">
 
                     {{ csrf_field() }}
-
+                    <div class="col-md-12">
                     <input type="text" name="name" id="name" readonly="readonly" value="{{ $name }}"><br>
-
+                    </div>
+                    <div class="col-md-12">
                     <textarea name="comment" id="comment" rows="4" cols="40" placeholder="レビュー（最大100字)" maxlength="100"></textarea>
+                    </div>
+                    <div class="col-md-12">
+                        シーン:
+                        <select id="scene" name="scene">
+                            <option value="1">誕生日</option>
+                            <option value="2">結婚記念日</option>
+                            <option value="3">クリスマス</option>
+                            <option value="4">出産祝い</option>
+                            <option value="5">結婚祝い</option>
+                            <option value="6">手土産</option>
+                            <option value="7">引っ越し</option>
+                            <option value="8">お中元＆お歳暮</option>
+                            <option value="9">父の日</option>
+                            <option value="10">母の日</option>
+                            <option value="11">敬老の日</option>
+                            <option value="12">卒業＆就職祝い</option>
+                            <option value="13">入学祝い</option>
 
+                        </select><br>
+                    </div>
+                    <div class="col-md-6">
+                        <input type="radio" name="wantgood" value="1" checked>もらって嬉しかったもの
+                        <input type="radio" name="wantgood" value="2">欲しいもの
+                    </div>
+                    <div class="col-md-6">
                     <p>評価
                     <select id="rate" name="rate" size="1">
                         <option value="5">★★★★★</option>
@@ -72,7 +97,10 @@
                         <option value="2">★★☆☆☆</option>
                         <option value="1">★☆☆☆☆</option>
                     </select></p><br>
+                    </div>
+                    <div class="col-md-12">
                     <label class="pink-button"><input type="submit">レビューを登録する</label>
+                        </div>
                 </form>
             </div>
 
