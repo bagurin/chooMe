d00cfaa7693ce6ff2a57a4f448ee083e0c7d38cd
まーしー <masashi0729@marcy.local>
2016-11-30 09:23:23 +0900

    メソッドの修正

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -7,21 +7,35 @@ use Request;
 use App\Getgoods;
 use App\User;
 use Auth;
+use Session;
 
 class ReviewController extends Controller
 {
 
+    // レビューページ作成
+    public function viewReview($name){
+
+        return view('review',compact('name'));
+
+    }
+
     // 入力された商品名を元に似た名前の商品があるかを検索し連想配列で返す
-    public function getDate()
+    public function getData()
     {
-        //$name = Request::get('name');
+        Session::put('name', Request::get('Search'));
 
-        $name = 'ak47';
+        //$list = Getgoods::where('name', 'LIKE', "%{$name}%")->paginate(1);
+        return redirect('/search-result/');
+    }
 
-        $list = Getgoods::where('name', 'LIKE', '%'.$name.'%')->paginate(10);
-        dd($list);
+    public function viewData(){
 
-        return view('search-result')->with('list', $list);
+        $name = Session::get('name');
+
+        $list = Getgoods::where('name', 'LIKE', "%".$name."%")->paginate(1);
+
+        return view('search-result', compact('list'));
+
     }
 
     // レビューをデータベースに格納
@@ -46,23 +60,24 @@ class ReviewController extends Controller
         }
 
 
-        // 商品名・コメント・評価点数取得
+        // 名前・コメント・評価点数・商品タイプ取得
         $syohin = Request::get('name');
         $comment = Request::get('comment');
         $rate = Request::get('rate');
+        $goods_type = (int)Request::input('wantgood');
+        $scene = Request::get('scene');
         // 商品id変数
         $getgoods_id = -1;
-
 
         //商品名からidを取得
         $getgoods_id = Getgoods::where('name', $syohin)->get('id');
 
-        $review = array('getgoods_id' => $getgoods_id, 'users_id' => $user_id,
-                            'scenes_id' => 1, 'comment' => $comment, 'rate' => (int)$rate);
+        $review = array('getgoods_id' => $getgoods_id, 'users_id' => $user_id, 'scenes_id' => $scene,
+            'goodstypes_id' => $goods_type,'comment' => $comment, 'rate' => (int)$rate);
         Review::create($review);
 
         // ランキングページにする際ゲストなら表示できるようにする
-        return redirect('/');
+        return redirect('/scene/');
     }
 
 }
