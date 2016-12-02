b792dc97bd9d2b52fdfd1fb17ae77e83118517fe
toroia0729 <y.masashi0729@gmail.com>
2016-12-03 01:04:20 +0900

    メソッドをわかりやすいものに修正/モデルに動的スコープを追加

--- app/Rank.php
+++ app/Rank.php
@@ -13,4 +13,25 @@ class Rank extends Model
         'ranking_no', 'getgoods_id', 'score', 'average_rate', 'patterns_id'
     ];
 
+    public function scopeJoin_goods($query){
+        return $query->join('getgoods', 'getgoods.id', '=', 'ranks.getgoods_id');
+    }
+
+    public function scopeLeftJoin_genres($query){
+        return $query->leftjoin('genres', 'genres.id', '=', 'getgoods.genres_id');
+    }
+
+    public function scopeSelect_rank($query){
+        return $query->select('ranks.ranking_no', 'ranks.getgoods_id',
+            'getgoods.name', 'getgoods.image', 'ranks.average_rate', 'genres.name as genres');
+    }
+
+    public function scopeWhere_rank($query, $pattern, $type){
+        return $query->where([['patterns_id', '=', $pattern], ['goodstypes_id', '=', $type]]);
+    }
+
+    public function scopeOrderBy_rank($query){
+        return $query->orderBy('ranks.ranking_no', 'asc');
+    }
+
 }
