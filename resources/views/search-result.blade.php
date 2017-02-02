a678d431478a7256af1f66e57af272ffde8880a4
西村勇希 <coca.xnhp@gmail.com>
2017-02-02 15:31:00 +0900

    無効有効修正

--- resources/views/search-result.blade.php
+++ resources/views/search-result.blade.php
@@ -113,7 +113,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
             parent.document.registform.genrename.style.backgroundColor = "#fff";
             parent.document.registform.comment.readOnly = false;
             parent.document.registform.comment.style.backgroundColor = "#fff";
-            parent.document.registform.rate.removeAttribute('disabled');
+            parent.document.registform.select.removeAttribute('disabled');
             parent.document.registform.select.style.backgroundColor = "#fff";
             parent.document.registform.good.removeAttribute('disabled');
             parent.document.registform.want.removeAttribute('disabled');
